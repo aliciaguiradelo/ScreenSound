8448849f8758f2fe409c81a4a4de495a41f19481
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-06-18 17:15:19 -0300

    adiciona resolução exercícios

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,6 +2,9 @@
 //ou seja, precisamos declarar as variáveis
 
 using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 //Para declarar variável, usa camelCase
 //Para declarar funções, usa PascalCase
@@ -125,6 +128,72 @@ void Exercicio03()
 
     Console.WriteLine($"Usuário {nomeUser} registrado com sucesso");
     
+}
+
+void Exercicio04()
+{
+    //Criar uma classe que representa uma conta bancária, com um número indicador, titular, saldo e senha.
+    //class Conta{
+    //public int id;
+    //public string titular;
+    //public int saldo;
+    //public int senha;
+    //}
+    //Criar um objeto do tipo Conta, adicionar dados e mostrar as informações titular e saldo no console,
+    //utilizando interpolação de strings.
+    //Conta conta = new Conta();
+    //conta.titular = "Gui Lima";
+    //conta.id = 1;
+    //conta.saldo = 20.00;
+    //conta.senha = 1234;
+    //Console.WriteLine("INFORMAÇÕES DA CONTA:");
+    //Console.WriteLine($"Titular: {conta.titular}");
+    //Console.WriteLine($"Saldo atual: {conta.saldo}")
+    //
+    //Desenvolver um método da classe Conta que exibe suas informações.
+    //public void exibirInformacoes(){
+    //    Console.WriteLine("INFORMAÇÕES DA CONTA:");
+    //    Console.WriteLine($"Titular: {this.titular}");
+    //    Console.WriteLine($"Saldo atual: {this.saldo}");
+    //}
+    //Desenvolver uma classe que modele um carro, e que contenha os métodos acelerar, frear e buzinar
+    //class Carro
+    //{
+    //public string fabricante;
+    //public string modelo;
+    //public int ano;
+    //public int quantidadePortas;
+    //public int velocidade = 0;
+
+    //public void exibirInformacoes()
+    //{
+    //    Console.WriteLine($"Informações do carro: {this.fabricante} {this.modelo}, {this.quantidadePortas} portas, {this.ano}");
+    //}
+
+    //public void acelerar()
+    //{
+    //    Console.WriteLine("Acelerando...");
+    //    if (velocidade < 100)
+    //    {
+    //        velocidade = velocidade + 5;
+    //    }
+    //}
+
+    //public void frear()
+    //{
+    //    Console.WriteLine("Freando...");
+    //    if (velocidade > 0)
+    //    {
+    //        velocidade = velocidade - 5;
+    //    }
+    //}
+
+    //public void buzinar()
+    //{
+    //    Console.WriteLine("Bi Bi");
+    //}
+    //}
+
 }
 void ExibirOpcoesMenu()
 {
